6156f90029cd2bd4aa59f0b91e4d44c911dd9683
nhjin <nhjin1106@gmail.com>
2022-03-06 12:35:54 +0900

    확인

---
+++ Oracle-HR1.sql
@@ -0,0 +1,8 @@
+select first_name, last_name, salary
+from employees;
+
+select first_name, salary+300
+from employees;
+
+--null은 이용할 수 없거나 지정되지 않거나 알 수 없거나 또는 적용할 수 없는 값입니다. 
+--null은 숫자 0이나 공백과는 다릅니다. 
